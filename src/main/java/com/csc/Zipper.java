b57f86255b76802049c4626ec878cb0bd138f12f
jgraessle <j_graessle@mail.harpercollege.edu>
2024-10-13 19:56:43 -0700

    Added new Zip method and tests of the Zip function
    The Zip method combines two lists into one where the lists' values alternate until only one list still has values which are all appended to the end of the new combined list. Tests were also added that ensure the Zip function works properly.

--- src/main/java/com/csc/Zipper.java
+++ src/main/java/com/csc/Zipper.java
@@ -1,5 +1,36 @@
 package com.csc;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Zipper {
+public class Zipper <T>{
+    List<T> combinedList = new ArrayList<>();
+    public List<T> Zip(List<T> list1, List<T> list2) {
+        
+        for (int i = 0; i < list1.size() + list2.size(); i++) {
 
+            if (i < list1.size()) {
+                combinedList.add(list1.get(i));
+            }
+            if (i < list2.size()) {
+                combinedList.add(list2.get(i));
+            }
+        }
+        return combinedList;
+    }
+
+public static void main(String args[]) {
+    String input;
+    List<String> stringList = List.of("one", "two", "three");
+    List<String> stringList2 = List.of("four", "five", "six", "seven");
+    List<Integer> intList =  List.of(1, 2, 3);
+    List<Integer> intList2 =  List.of(4, 5, 6, 7);
+    List<Double> doubleList = List.of(1.0, 2.0, 3.0);
+    List<Double> doubleList2 = List.of(4.0, 5.0, 6.0, 7.0);
+    Zipper<String> zipString = new Zipper();
+    System.out.println(zipString.Zip(stringList, stringList2));
+    Zipper<Integer> zipInteger = new Zipper();
+    System.out.println(zipInteger.Zip(intList, intList2));
+    Zipper<Double> zipDouble = new Zipper();
+    System.out.println(zipDouble.Zip(doubleList, doubleList2));
+}
 }
--- src/test/java/com/csc/ZipperTest.java
+++ src/test/java/com/csc/ZipperTest.java
@@ -1,21 +1,37 @@
 package com.csc;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ZipperTest {
-
-  Zipper zipper;
-
+  Zipper<String> zipString = new Zipper();
+  Zipper<Integer> zipInteger = new Zipper();
+  Zipper<Double> zipDouble = new Zipper();
+  List<String> stringList = List.of("one", "two", "three");
+  List<String> stringList2 = List.of("four", "five", "six", "seven");
+  List<String> stringListTest = List.of("one", "four", "two", "five", "three", "six", "seven");
+  List<Integer> intList =  List.of(1, 2, 3);
+  List<Integer> intList2 =  List.of(4, 5, 6, 7);
+  List<Integer> intListTest =  List.of(1, 4, 2, 5, 3, 6, 7);
+  List<Double> doubleList = List.of(1.0, 2.0, 3.0);
+  List<Double> doubleList2 = List.of(4.0, 5.0, 6.0, 7.0);
+  List<Double> doubleListTest =  List.of(1.0, 4.0, 2.0, 5.0, 3.0, 6.0, 7.0);
   @BeforeEach
   void setUp() {
-    zipper = new Zipper();
   }
 
   @Test
-  void itWorks() {
-    assertEquals(true, true);
+  void ZipForIntegers() {
+    assertEquals(intListTest, zipInteger.Zip(intList, intList2));
+  }
+  @Test
+  void ZipForDoubles() {
+    assertEquals(doubleListTest, zipDouble.Zip(doubleList, doubleList2));
+  }
+  @Test
+  void ZipforStrings() {
+    assertEquals(stringListTest, zipString.Zip(stringList, stringList2));
   }
 }
